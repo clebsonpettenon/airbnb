17aa6f120501ca0fe5140d859122d0780bf500f2
Clebson Pettenon <clebsonpettenon@gmail.com>
2020-05-07 19:20:24 -0300

    Criando funcao para abrir e fechar o dropdown

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-let quartosAirBnb = ()=>{
+let dropdown = () => {
+    let x = document.getElementById('dropdown');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+let quartosAirBnb = () => {
     let url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState === 4 && xhr.status === 200){
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             let json = JSON.parse(xhr.responseText);
             carregarQuartos(json);
         }
@@ -11,13 +20,15 @@ let quartosAirBnb = ()=>{
     xhr.send();
 }
 
-let carregarQuartos = (quarto)=>{
-    for(let i=0; i < quarto.length; i++){
-        let tag = '<div>'+
-                  '<img src=' + quarto[i].photo + '>'+
-                  '<p>' + quarto[i].name + '</p>'+
-                  '</div>';
-        document.querySelector('#quartos').insertAdjacentHTML('beforeend', tag);
+let carregarQuartos = (quarto) => {
+    for (let i = 0; i < quarto.length; i++) {
+        let tag = '<div class="card">' +
+            '<img src=' + quarto[i].photo + '>' +
+            '<p>' + quarto[i].property_type + '</p>' +
+            '<p>' + quarto[i].name + '</p>' +
+            '<p>' + quarto[i].price + '</p>' +
+            '</div>';
+        document.querySelector('.quartos').insertAdjacentHTML('beforeend', tag);
     }
 }
 
